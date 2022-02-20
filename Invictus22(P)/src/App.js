75d37b39263d1c58e367dcbb4ab893d72599bc4f
Aditi bhosale <84183428+AditiBhosale2012@users.noreply.github.com>
2022-02-20 10:25:41 +0530

    Add files via upload

--- Invictus22(P)/src/App.js
+++ Invictus22(P)/src/App.js
@@ -1,86 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Start from './components/start';
-import Question from './components/question';
-import End from './components/end';
-import quizData from './data/quiz.json';
-import Modal from './components/answers';
-import Home from './home';
-import Quiz from './quiz';
-import Community from './community';
-import FAQ from './faq';
-
-
-let interval;
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import ChatBot from './chatBot';
 
 const App = () => {
-  const [step, setStep] = useState(1);
-  const [activeQuestion, setActiveQuestion] = useState(0);
-  const [answers, setAnswers] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [time, setTime] = useState(0);
-
-  useEffect(() => {
-    if(step === 3) {
-      clearInterval(interval);
-    }
-  }, [step]);
-
-  const quizStartHandler = () => {
-    setStep(2);
-    interval = setInterval(() => {
-      setTime(prevTime => prevTime + 1);
-    }, 1000);
-  }
-
-  const resetClickHandler = () => {
-    setActiveQuestion(0);
-    setAnswers([]);
-    setStep(2);
-    setTime(0);
-    interval = setInterval(() => {
-      setTime(prevTime => prevTime + 1);
-    }, 1000);
-  }
-
   return (
-    
-    <div className="App">
-      <div className='nav'>
-      <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/home' exact component={Home} />
-        <Route path='/quiz' component={Quiz} />
-        <Route path='/community' component={Community} />
-        <Route path='/faq' component={FAQ} />
-      </Switch>
-    </Router>
-    </div>
-      {step === 1 && <Start onQuizStart={quizStartHandler} />}
-      {step === 2 && <Question 
-        data={quizData.data[activeQuestion]}
-        onAnswerUpdate={setAnswers}
-        numberOfQuestions={quizData.data.length}
-        activeQuestion={activeQuestion}
-        onSetActiveQuestion={setActiveQuestion}
-        onSetStep={setStep}
-      />}
-      {step === 3 && <End
-      results={answers}
-      data={quizData.data}
-      onReset={resetClickHandler}
-      onAnswerCheck={() => setStep(4)}
-      time={time}
-      />}
-      {step === 4 && <Modal 
-      results={answers}
-      data={quizData.data}
-      />}
-    </div>
+    <SafeAreaView>
+      <ChatBot />
+    </SafeAreaView>
   );
-}
+};
 
-export default App; 
+export default App;
---
+++ Invictus22(P)/src/chatBot/data.js
@@ -0,0 +1,51 @@
+export const data = [
+    {
+      question: 'hi',
+      answer: 'Hello',
+    },
+    {
+      question: 'hello',
+      answer: 'Hi',
+    },
+    {
+      question: 'hi bot',
+      answer: 'Hello',
+    },
+    {
+      question: 'hello bot',
+      answer: 'Hi',
+    },
+    {
+      question: 'how are you',
+      answer: 'I am fine',
+    },
+    {
+      question: 'what is your age',
+      answer: 'I am developed today',
+    },
+    {
+      question: 'what is your name',
+      answer: 'I am bot',
+    },
+    {
+       question: 'should i use menstrual cups',
+       answer: 'Menstrual cups are a better alternative to synthetic sanitary napkins. It is a reusable menstrual hygiene product, which can effectively deal with leaks as well as the persistent problem of disposal of non-biodegradable sanitary napkins.',
+    },
+    {
+       question: 'why do i experience cramps during periods',
+       answer: 'Cramps are painful and throbbing sensation experienced by many women during periods. Menstrual cramps are concentrated in the lower abdomen. During periods, the uterine wall contracts to expel its lining. This contraction is brought about by prostaglandins, which are mediators of inflammatory response. Excess of prostaglandins can cause severe menstrual cramps. ',
+    },
+    {
+        question: 'will exercise stop my period',
+        answer: 'Being active can cause menstrual abnormalities, but it takes a lot of very intense exercise. Usually, only women who compete professionally experience changes in their periods. Why this happens isnt clearly understood, but is likely a combination of extreme exercise, an increase in lean body mass, not eating enough or getting the proper nutrients, and stress related to competition.',
+    },
+    {
+      question: 'can i still play sports when i am menstruating',
+      answer: 'Girls worry that menstruation might interfere with their everyday activities, but theres no reason you cant participate in sports or other extracurricular activities just because she is menstruating.',
+    },
+    {
+      question: 'what makes sanitary napkins so absorbent',
+      answer: 'Initially, sanitary napkins were made of cotton-based absorbent bound by gauge and a waterproof lining. These evolved into ultra-thin synthetic sanitary napkins which use Super Absorbent Polymer (SAP) with Polyethylene back. SAP is a petroleum-based product that can absorb liquids many times its weight and convert into gel. SAP is added to a compressed cotton absorbent core to increase its absorption capacity.',
+    },
+
+  ];
